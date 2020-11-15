eb15b80db62632b68a84aa0c258c2630f2ddb2fa
shai <eliav.english@gmail.com>
2020-11-15 12:14:49 +0200

    reorganized css and js files

--- send-msg.js
+++
@@ -1,5 +0,0 @@
-function sendMsg() {
-    let rawMsg = document.getElementById("messageValue").value;
-    let msg = encodeURIComponent(rawMsg);
-    window.open(`https://wa.me/972542349294?text=${msg}`,'_blank');
-}
